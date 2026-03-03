040113f62371bc5643ed5595372fe638067528ad
Basampelliadharsh <adharshbasampalli@gmail.com>
2026-03-03 07:59:07 +0530

    calci practice

---
+++ javascript-practice/calci.js
@@ -0,0 +1,17 @@
+const num1Input = document.querySelector("#num1");
+const num2Input = document.querySelector("#num2");
+const addBtn = document.querySelector("#addBtn");
+const subBtn = document.querySelector("#subBtn");
+const result = document.querySelector("#result");
+addBtn.addEventListener("click", function () {
+  const num1 = Number(num1Input.value);
+  const num2 = Number(num2Input.value);
+  const sum = num1 + num2;
+  result.textContent = "The sum of the two numbers is: " + sum;
+});
+subBtn.addEventListener("click", function () {
+  const num1 = Number(num1Input.value);
+  const num2 = Number(num2Input.value);
+  const difference = num1 - num2;
+  result.textContent = "The difference of the two numbers is: " + difference;
+});
